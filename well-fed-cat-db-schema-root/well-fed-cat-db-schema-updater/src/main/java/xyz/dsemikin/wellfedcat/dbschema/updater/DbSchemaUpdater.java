8d38dcc57bba0ee564fdf2c992a32f5fcd0b7c5b
Dmitrii Semikin <dmitry.semikin@gmail.com>
2021-05-01 00:24:16 +0200

    Add project db-schema-updater

---
+++ well-fed-cat-db-schema-root/well-fed-cat-db-schema-updater/src/main/java/xyz/dsemikin/wellfedcat/dbschema/updater/DbSchemaUpdater.java
@@ -0,0 +1,7 @@
+package xyz.dsemikin.wellfedcat.dbschema.updater;
+
+public class DbSchemaUpdater {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
