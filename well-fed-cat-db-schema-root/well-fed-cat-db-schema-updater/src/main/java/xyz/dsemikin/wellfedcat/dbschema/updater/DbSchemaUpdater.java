5485d91184a37922be6795d9cfcfa6ec2585ebd3
Dmitrii Semikin <dmitry.semikin@gmail.com>
2021-05-01 00:44:48 +0200

    Implement very simple version of updater, which uses scripts from db-schema module.

--- well-fed-cat-db-schema-root/well-fed-cat-db-schema-updater/src/main/java/xyz/dsemikin/wellfedcat/dbschema/updater/DbSchemaUpdater.java
+++ well-fed-cat-db-schema-root/well-fed-cat-db-schema-updater/src/main/java/xyz/dsemikin/wellfedcat/dbschema/updater/DbSchemaUpdater.java
@@ -1,7 +1,25 @@
 package xyz.dsemikin.wellfedcat.dbschema.updater;
 
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationInfo;
+import org.flywaydb.core.api.MigrationInfoService;
+
 public class DbSchemaUpdater {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final String dbUrl = "jdbc:h2:~/test_db";
+        final String user = "sa";
+        final String password = "sa";
+        Flyway flyway = Flyway.configure()
+                .dataSource(dbUrl, user, password)
+                .locations("classpath:sql")
+                .load();
+        MigrationInfoService infoService = flyway.info();
+
+        for (MigrationInfo info : infoService.all()) {
+            System.out.println("Version: " + info.getVersion()
+                    + "; description: " + info.getDescription()
+                    + "; state: " + info.getState()
+            );
+        }
     }
 }
