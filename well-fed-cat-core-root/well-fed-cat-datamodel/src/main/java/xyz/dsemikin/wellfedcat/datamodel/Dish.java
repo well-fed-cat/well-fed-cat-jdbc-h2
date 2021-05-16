d4b1376a74ef9a4d5ed82ad5ef03a74e6a0cce5c
Dmitrii Semikin <dmitry.semikin@gmail.com>
2021-05-16 21:38:22 +0200

    Introduce Dish.publicId.
    It is not yet included there, where dishes are
    created, so these places will not even build.
    
    Also not included into the DB schema definition.

--- well-fed-cat-core-root/well-fed-cat-datamodel/src/main/java/xyz/dsemikin/wellfedcat/datamodel/Dish.java
+++ well-fed-cat-core-root/well-fed-cat-datamodel/src/main/java/xyz/dsemikin/wellfedcat/datamodel/Dish.java
@@ -4,10 +4,30 @@ import java.io.Serializable;
 import java.util.Set;
 
 public record Dish(
+        String publicId,
         String name,
         Set<MealTime> suitableForMealTimes
 )
     implements Serializable
 {
-    // nothing here
+
+    public static final int MAX_DISH_NAME_LENGTH = 100; // must be coherent with DB schema
+
+    public Dish {
+        final boolean publicIdIsOk =
+                publicId.codePoints().allMatch(c ->
+                        c < 128 &&
+                                (Character.isLetter(c)
+                                        || Character.isDigit(c)
+                                        || c == Character.codePointAt("_", 0)
+                                )
+                );
+        if (!publicIdIsOk) {
+            throw new IllegalArgumentException("publicId must be composed of digits, ascii chars or underscore.");
+        }
+
+        if (name.length() > MAX_DISH_NAME_LENGTH) {
+            throw new IllegalArgumentException("Max allowed length of dish name is " + MAX_DISH_NAME_LENGTH);
+        }
+    }
 }
