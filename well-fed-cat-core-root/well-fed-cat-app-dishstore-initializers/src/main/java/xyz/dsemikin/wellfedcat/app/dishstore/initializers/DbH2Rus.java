0d770808f9d54a4e4605c14d0ee3cf2b5563ab85
Dmitrii Semikin <dmitry.semikin@gmail.com>
2021-05-18 21:16:07 +0200

    Repair apps, which initialize DB with Russian dishes.
    both: cyrillic and translit versions.

--- well-fed-cat-core-root/well-fed-cat-app-dishstore-initializers/src/main/java/xyz/dsemikin/wellfedcat/app/dishstore/initializers/DbH2Rus.java
+++ well-fed-cat-core-root/well-fed-cat-app-dishstore-initializers/src/main/java/xyz/dsemikin/wellfedcat/app/dishstore/initializers/DbH2Rus.java
@@ -14,43 +14,41 @@ public class DbH2Rus {
         final var dishStore = new DishStoreDbH2(dbFilePath);
 
         // TODO: Fix definition of dish.
-        dishStore.add(new Dish("ovsyanaya_kasha", "Овсяная каша", newHashSet(MealTime.BREAKFAST)));
-//        dishStore.add(new Dish("Манная каша", newHashSet(MealTime.BREAKFAST)));
-//        dishStore.add(new Dish("Бутерброды",                                   newHashSet(MealTime.BREAKFAST, MealTime.SUPPER)));
-//        dishStore.add(new Dish("Овсяные хлопья/мюсли",                         newHashSet(MealTime.BREAKFAST, MealTime.SUPPER)));
-//        dishStore.add(new Dish("Блинчики",                                     newHashSet(MealTime.BREAKFAST, MealTime.SUPPER)));
-//        dishStore.add(new Dish("Гренки",                                       newHashSet(MealTime.BREAKFAST, MealTime.SUPPER)));
-//        dishStore.add(new Dish("Вареные яйца",                                 newHashSet(MealTime.BREAKFAST, MealTime.SUPPER)));
-//        dishStore.add(new Dish("Мягкие вафли",                                 newHashSet(MealTime.BREAKFAST)));
-//        dishStore.add(new Dish("Рыба с песто и фасолью.",                      newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Оливье",                                       newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Крабовый салат",                               newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Пицца",                                        newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("На вынос",                                     newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Гуляш",                                        newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Рыба тандури",                                 newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Овощи вок с курицей",                          newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Красная фасоль с фаршем",                      newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Зеленая фасоль с беконом",                     newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Тортелини",                                    newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Пельмени",                                     newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Паста с морепродуктами",                       newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Паста карбонара",                              newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Паста болоньезе (с фаршем и соусом из банки)", newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Картошка жаренная с грибами",                  newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Курица жаренная",                              newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Курица запеченная с сыром и грибами",          newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Фаршированные перцы",                          newHashSet(MealTime.LUNCH)));
-//        dishStore.add(new Dish("Творог",                                       newHashSet(MealTime.SUPPER)));
-//        dishStore.add(new Dish("Йогурт",                                       newHashSet(MealTime.SUPPER)));
-//        dishStore.add(new Dish("Сырники",                                      newHashSet(MealTime.SUPPER)));
-//        dishStore.add(new Dish("Яичница",                                      newHashSet(MealTime.LUNCH, MealTime.SUPPER)));
-//        dishStore.add(new Dish("Омлет",                                        newHashSet(MealTime.LUNCH, MealTime.SUPPER)));
-//        dishStore.add(new Dish("Сосиски",                                      newHashSet(MealTime.LUNCH, MealTime.SUPPER)));
-//        dishStore.add(new Dish("Сыр в духовке",                                newHashSet(MealTime.SUPPER)));
-//        dishStore.add(new Dish("Творожная запеканка",                          newHashSet(MealTime.SUPPER)));
-
-
+        dishStore.add(new Dish("ovsyanaja_kasha", "Овсяная каша", newHashSet(MealTime.BREAKFAST)));
+        dishStore.add(new Dish("mannaja_kasha", "Манная каша", newHashSet(MealTime.BREAKFAST)));
+        dishStore.add(new Dish("buterbrody", "Бутерброды", newHashSet(MealTime.BREAKFAST, MealTime.SUPPER)));
+        dishStore.add(new Dish("ovsjanye_hlopja", "Овсяные хлопья/мюсли", newHashSet(MealTime.BREAKFAST, MealTime.SUPPER)));
+        dishStore.add(new Dish("blinchiki", "Блинчики", newHashSet(MealTime.BREAKFAST, MealTime.SUPPER)));
+        dishStore.add(new Dish("grenki", "Гренки", newHashSet(MealTime.BREAKFAST, MealTime.SUPPER)));
+        dishStore.add(new Dish("varjonyje_jaica", "Вареные яйца", newHashSet(MealTime.BREAKFAST, MealTime.SUPPER)));
+        dishStore.add(new Dish("mjagkije_vafli", "Мягкие вафли", newHashSet(MealTime.BREAKFAST)));
+        dishStore.add(new Dish("ryba_s_pesto", "Рыба с песто и фасолью.", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("olivje", "Оливье", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("krabovyj_salat", "Крабовый салат", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("picca", "Пицца", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("na_vynos", "На вынос", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("guljash", "Гуляш", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("ryba_tandury", "Рыба тандури", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("vok_s_kuricej", "Овощи вок с курицей", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("krasnaja_fasol_s_farshem", "Красная фасоль с фаршем", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("zelenaja_fasol_s_bekonom", "Зеленая фасоль с беконом", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("tortelini", "Тортелини", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("pelmeni", "Пельмени", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("pasta_s_moreproduktami", "Паста с морепродуктами", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("pasta_karbonara", "Паста карбонара", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("pasta_bolonese", "Паста болоньезе (с фаршем и соусом из банки)", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("kartoshka_s_gribami", "Картошка жаренная с грибами", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("kurica_zharenaja", "Курица жаренная", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("kurica_s_gribami_i_syrom", "Курица запеченная с сыром и грибами", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("farshirovannyje_percy", "Фаршированные перцы", newHashSet(MealTime.LUNCH)));
+        dishStore.add(new Dish("tvorog", "Творог", newHashSet(MealTime.SUPPER)));
+        dishStore.add(new Dish("jogurt", "Йогурт", newHashSet(MealTime.SUPPER)));
+        dishStore.add(new Dish("syrniki", "Сырники", newHashSet(MealTime.SUPPER)));
+        dishStore.add(new Dish("jaichnica", "Яичница", newHashSet(MealTime.LUNCH, MealTime.SUPPER)));
+        dishStore.add(new Dish("omlet", "Омлет", newHashSet(MealTime.LUNCH, MealTime.SUPPER)));
+        dishStore.add(new Dish("sosiski", "Сосиски", newHashSet(MealTime.LUNCH, MealTime.SUPPER)));
+        dishStore.add(new Dish("syr_v_duhovke", "Сыр в духовке", newHashSet(MealTime.SUPPER)));
+        dishStore.add(new Dish("tvorozhnaja_zapekanka", "Творожная запеканка", newHashSet(MealTime.SUPPER)));
     }
 
     @SafeVarargs
